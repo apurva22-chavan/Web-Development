4d403cb375c300c0d3274c12f626f7a9a4516bee
apurva22-chavan <apurvachavan8910@gmail.com>
2025-10-21 00:21:26 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 alert("Welcome to Sigma Web Development Course")
+
+console.log("Code is running....")
+console.log("Code is also running....")
+console.log("Code is looking like a wow....")
+
+var a=prompt("Enter your name")
+var isTrue=confirm("Are you enjoying Javascript?")
+if(isTrue){
+    console.log("Best")
+}
+else{
+    console.log("Worst")
+}
+console.log("Your name is"+ a)
+
+document.title="Hey I am good"
+document.body.style.backgroundColor="red"npm
