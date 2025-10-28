7ce60109c34b6878a22ce8bb433b4c07c71349bc
apurva22-chavan <apurvachavan8910@gmail.com>
2025-10-28 21:15:08 +0530

    Add files via upload

---
+++ s1.js
@@ -0,0 +1,36 @@
+console.log("Practice fir JS")
+let a=5;
+let b=6;
+let c="abc"
+// console.log(a+b)
+// console.log(a+b+8)
+console.log(typeof a,typeof b, typeof c)
+
+const a1 =6; //value of const i.e a1 cannot be changed (a1=a1+4)
+{
+    let a=66;
+    console.log(a)
+}
+console.log(a)
+
+//Primitive data types :----
+//null , number , string , symbol , undefined , boolena , BigInt
+
+
+let x="abc";
+let y=22;
+let z=3.55;
+const p=true;
+let q=undefined;
+let r=null;
+console.log(x,y,z,p,q,r)
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r)
+
+let o={
+    "name":"Harry",
+    "job code":5600,
+    "is_handsome": true
+}
+console.log(o);
+o.salary="100cores";
+console.log(o);
