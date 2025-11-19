d2a75699553bc34666377e5f7176744e11e83b1f
apurva22-chavan <apurvachavan8910@gmail.com>
2025-11-19 10:46:20 +0530

    Add files via upload

---
+++ js_loops.js
@@ -0,0 +1,45 @@
+// console.log("I am tutorial on Loops")
+// let a=1;
+// for(let i=0;i<100;i++){
+//     console.log(i+1);
+// }
+// let obj={
+//     name:"abc";
+//     role:"Programmar";
+//     company:"XYZ";
+}
+// for (const key in obj) { 
+//     const element = obj[key];
+//     console.log(key,element)
+    
+// }
+
+// for (const key in obj) { 
+//     console.log(key,element)
+    
+// }
+
+// for (const c of "Apurva"){
+//     console.log(c)
+}
+// for (const key in object) {
+//     if (!Object.hasOwn(object, key)) continue;
+    
+//     const element = object[key];
+    
+    
+// }
+// for (const element of object) {
+    
+// }
+
+// let i=0;
+// while(i<6){
+//     console.log(i)
+//     i++
+// } 
+let i=0;
+do {
+    console.log(i)
+    i++
+} while (i<6);
