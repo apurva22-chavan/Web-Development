d81959228b58b5e52b56dca02e46560aaad72057
apurva22-chavan <apurvachavan8910@gmail.com>
2025-11-24 20:43:38 +0530

    Add files via upload

---
+++ cps.js
@@ -0,0 +1,21 @@
+console.log("Hello World!")
+
+document.body.firstElementChild
+document.body.firstElementChild.childNodes
+document.body.firstElementChild.children
+
+//Table Links
+//Certain DOM elements may provide additional properties specific to their type for convenience.
+//Table element support the following properties:
+/*
+table.row - collection of elements
+table.caption - reference to <caption>
+table.thead - reference to <thead>
+table.tfoot - reference to <tfoot>
+table.tBodies -  collection of <tbody> elements
+tbody.rows - collectioon of <tr> inside.
+tr.cells - Collection of td and th
+tr.sectionRowIndex - Index of tr inside enclosing element
+tr.cellIndex - no of cells inside enclosing <tr>
+
+*/
